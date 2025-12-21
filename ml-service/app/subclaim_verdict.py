54dfb8ae53b035c0d7518ebfa2c44f2c95ff434a
Harshal Raj <harshalraj27@gmail.com>
2025-12-21 23:55:48 +0530

    2nd Commit on 21st Dec

--- ml-service/app/subclaim_verdict.py
+++ ml-service/app/subclaim_verdict.py
@@ -1,1 +1,22 @@
 from .config import *
+
+def get_verdict(aggregator_output):
+    total_strength = aggregator_output['total_strength']
+    support_strength = aggregator_output['support_strength']
+    contradict_strength = aggregator_output['contradict_strength']
+
+    support_ratio = support_strength / total_strength
+    contradict_ratio = contradict_strength / total_strength
+
+    if(total_strength<0.2):
+        label = "INCONCLUSIVE"
+    elif (support_ratio > 0.35 and contradict_ratio > 0.35):
+        label = "MIXED"
+    elif(support_strength>=2 and contradict_strength<0.3):
+        label = "SUPPORT"
+    elif(support_strength<0.3 and contradict_strength>=2):
+        label = "CONTRADICT"
+    else:
+        label = "INCONCLUSIVE"
+
+    return label, total_strength, support_strength, contradict_strength
